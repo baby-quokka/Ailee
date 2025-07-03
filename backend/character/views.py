01e91936d3d3eec0b480cbca4ec454f26c5b074e
SeongJun <jun05202569@gmail.com>
2025-07-03 14:51:40 +0900

    remove local backend

--- backend/character/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.generics import get_object_or_404, ListCreateAPIView
-from rest_framework.pagination import PageNumberPagination
-# Create your views here.
